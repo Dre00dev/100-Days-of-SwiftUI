85a79894481fa4a3d017c3eaafa6884b8fd4689b
Andres Pulgarin <andrespulgarin00@outlook.com>
2023-02-15 18:03:02 -0500

    Day 2 basics of syntax

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,27 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+
+
+//string interpolation example
+let age = 22
+let name = "Andres"
+let greeting = "Hello, my name is \(name).  I am \(age) years old. "
+
+print (greeting)
+
+
+let text = "The number of characters in this string is:"
+print(text + String(text.count))
+
+var multiple = 120.isMultiple(of: 3)
+print(multiple)// true
+multiple.toggle() //or multiple = !multiple
+print(multiple)  //false
+
+let fileName = "Andres.jpg"
+
+var jpgCHeck = fileName.hasSuffix(".jpg")
+
+print (jpgCHeck)// true
+
+//expected output
