74ad73d4ff514544a2d35df54e35eba910003533
Andres Pulgarin <andrespulgarin00@outlook.com>
2023-03-05 13:13:47 -0500

    Day-20

---
+++ Intro To Swift/GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Andres Pulgarin on 3/4/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
