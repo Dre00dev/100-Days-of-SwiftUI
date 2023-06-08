283077c02d1e206a9ab5df91d5b9ab3dfbd01835
Andres Pulgarin <andrespulgarin00@outlook.com>
2023-06-08 14:01:31 -0400

    day-62

---
+++ Intro To Swift/InstaFilter/InstaFilter/InstaFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaFilterApp.swift
+//  InstaFilter
+//
+//  Created by Andres Pulgarin on 6/5/23.
+//
+
+import SwiftUI
+
+@main
+struct InstaFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
